d6b2374697e9e9cc4031ea4142ec1d47558d84b9
Harvey Ho <harvey_1997@hotmail.ca>
2020-12-01 18:16:29 -0500

    Add project files.

---
+++ ShoppingWebService/ShoppingService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ShoppingWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShoppingService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ShoppingService.svc or ShoppingService.svc.cs at the Solution Explorer and start debugging.
+    public class ShoppingService : IShoppingService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
