e8c6419658872dc0770f3d628bf12ec4b6e28640
Harvey Ho <harvey_1997@hotmail.ca>
2020-12-02 18:23:29 -0500

    Added comments

--- ShoppingWebService/IShoppingService.cs
+++ ShoppingWebService/IShoppingService.cs
@@ -5,6 +5,11 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
+/*
+ * Edit by Harvey Ho
+ * December 12 2020, 6:18pm EST
+ */
+
 namespace ShoppingWebService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShoppingService" in both code and config file together.
@@ -12,21 +17,27 @@ namespace ShoppingWebService
     public interface IShoppingService
     {
         [OperationContract]
+        //Returns all products in the Products table
         List<Product> GetAllProducts();
 
         [OperationContract]
+        //Returns a product based on it's id from the Product table
         Product GetAProduct(int id);
 
         [OperationContract]
+        //add a product to the Product table
         void AddProduct(Product product);
 
         [OperationContract]
+        //edit a product from the Product table
         void EditProduct(Product product);
 
         [OperationContract]
+        //remove a product from the Product table
         void DeleteProduct(Product product);
 
         [OperationContract]
+        //get products based on price from the Product table
         List<Product> GetProductsByPrice(double price);
     }
 }
--- ShoppingWebService/ShoppingService.svc.cs
+++ ShoppingWebService/ShoppingService.svc.cs
@@ -6,6 +6,11 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
+/*
+ * Edit by Harvey Ho
+ * December 12 2020, 6:18pm EST
+ */
+
 namespace ShoppingWebService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShoppingService" in code, svc and config file together.
@@ -14,34 +19,40 @@ namespace ShoppingWebService
     {
         ShoppingDBEntities context = new ShoppingDBEntities();
 
+        //add a product to the Product table
         public void AddProduct(Product product)
         {
             context.Products.Add(product);
             context.SaveChanges();
         }
 
+        //remove a product from the Product table
         public void DeleteProduct(Product product)
         {
             context.Products.Remove(context.Products.Find(product.Id));
             context.SaveChanges();
         }
 
+        //edit a product from the Product table
         public void EditProduct(Product product)
         {
             context.Entry(product).State = EntityState.Modified;
             context.SaveChanges();
         }
 
+        //Returns all products in the Products table
         public List<Product> GetAllProducts()
         {
             return context.Products.ToList();
         }
 
+        //Returns a product based on it's id from the Product table
         public Product GetAProduct(int id)
         {
             return context.Products.SingleOrDefault(p => p.Id == id);
         }
 
+        //get products based on price from the Product table
         public List<Product> GetProductsByPrice(double price)
         {
             return context.Products.Where(p => p.Price == price).Select(p => p).ToList();
