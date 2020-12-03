3e131bed7b6ad57c8bf35141b558237da86df361
Harvey Ho <harvey_1997@hotmail.ca>
2020-12-03 18:25:48 -0500

    created test client to consume GetAllProducts

---
+++ ShoppingClient/Controllers/ShoppingController.cs
@@ -0,0 +1,91 @@
+﻿using ShoppingClient.ShoppingServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingClient.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        ShoppingServiceClient client = new ShoppingServiceClient();
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            return View(client.GetAllProducts());
+        }
+
+        // GET: Shopping/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Shopping/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shopping/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Shopping/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Shopping/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Shopping/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Shopping/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
