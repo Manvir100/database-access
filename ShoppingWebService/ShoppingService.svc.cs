9d2044a594fbdad1cd3efdd8e6623fdfda39b112
HoHarvey <63257951+HoHarvey@users.noreply.github.com>
2020-12-10 22:14:04 -0500

    Add files via upload

--- ShoppingWebService/ShoppingService.svc.cs
+++ ShoppingWebService/ShoppingService.svc.cs
@@ -22,36 +22,82 @@ namespace ShoppingWebService
         //add a product to the Product table
         public void AddProduct(Product product)
         {
-            context.Products.Add(product);
+            context.Products.Add(product); //adds a product to the Product table
+            context.SaveChanges(); //save those changes
+        }
+        public void AddUser(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+        public void AddUserInfos(UserInfo userInfo)
+        {
+            context.UserInfoes.Add(userInfo);
             context.SaveChanges();
         }
 
         //remove a product from the Product table
-        public void DeleteProduct(Product product)
+        public void DeleteProduct(int id)
         {
-            context.Products.Remove(context.Products.Find(product.Id));
+            context.Products.Remove(context.Products.Find(id)); //first find the product by ID then remove the product from table
+            context.SaveChanges(); //save those changes
+        }
+        public void DeleteUser(int id)
+        {
+            context.Users.Remove(context.Users.Find(id));
+            context.SaveChanges();
+        }
+        public void DeleteUserInfos(int id)
+        {
+            context.UserInfoes.Remove(context.UserInfoes.Find(id)); 
             context.SaveChanges();
         }
 
         //edit a product from the Product table
         public void EditProduct(Product product)
         {
-            context.Entry(product).State = EntityState.Modified;
-            context.SaveChanges();
+            context.Entry(product).State = EntityState.Modified; //Using EntityState to find what was modified from the old state and change the state
+            context.SaveChanges(); //save those changes
         }
 
+        public void EditUser(User user)
+        {
+            context.Entry(user).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+        public void EditUserInfos(UserInfo userInfo)
+        {
+            context.Entry(userInfo).State = EntityState.Modified;
+            context.SaveChanges();
+        }
         //Returns all products in the Products table
         public List<Product> GetAllProducts()
         {
-            return context.Products.ToList();
+            return context.Products.ToList(); //returns the Products table to a list
         }
-
+        //Returns all user in the Users table
+        public List<User> GetAllUsers()
+        {
+            return context.Users.ToList();
+        }        
+        //Returns all userinfos in the Userinfo table
+        public List<UserInfo> GetAllUserInfos()
+        {
+            return context.UserInfoes.ToList();
+        }
         //Returns a product based on it's id from the Product table
         public Product GetAProduct(int id)
         {
-            return context.Products.SingleOrDefault(p => p.Id == id);
+            return context.Products.SingleOrDefault(p => p.Id == id); //find a product based on it's ID
         }
-
+        public User GetAUser(int id)
+        {
+            return context.Users.SingleOrDefault(p => p.Id == id);
+        }
+        public UserInfo GetAUserInfos(int id)
+        {
+            return context.UserInfoes.SingleOrDefault(p => p.Id == id);
+        }
         public User GetUserByUsername(string username)
         {
             return context.Users.SingleOrDefault(u => u.Username == username);
@@ -60,7 +106,7 @@ namespace ShoppingWebService
         //get products greater than and equal to price from the Product table
         public List<Product> GetProductsGreaterThanOrEqualToPrice(double price)
         {
-            return context.Products.Where(p => p.Price >= price).Select(p => p).ToList();
+            return context.Products.Where(p => p.Price >= price).Select(p => p).ToList(); //return products that have a greater or equal price to a list
         }
     }
 }
